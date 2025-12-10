f6422d77d64c81842a773c5b2f7f79debc65e1a0
Raul AKbarullah <raulakbar1905@gmail.com>
2025-12-10 14:26:05 +0700

    progress 5

--- lib/core/widgets/logo_widget.dart
+++ lib/core/widgets/logo_widget.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
 
+// class BPSLogoWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 400,
+//       height: 80,
+//       decoration: BoxDecoration(
+//         color: Colors.white.withOpacity(0.4),
+//         boxShadow: [
+//           BoxShadow(
+//             color: Colors.black.withOpacity(0.1),
+//             blurRadius: 8,
+//             offset: Offset(0, 2),
+//           ),
+//         ],
+//         image: DecorationImage(
+//           image: AssetImage('assets/images/logo_2.png'),
+//           fit: BoxFit.cover,
+//         ),
+//       ),
+//     );
+//   }
+// }
 class BPSLogoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 80,
-      height: 160,
+      width: 400,
+      height: 70,
       decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.4),
+        color: Colors.white.withOpacity(0.0),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.1),
+            color: Colors.black.withOpacity(0.0),
             blurRadius: 8,
             offset: Offset(0, 2),
           ),
         ],
-        image: DecorationImage(
-          image: AssetImage('assets/images/logo_2.png'),
-          fit: BoxFit.cover, // menjaga proporsi gambar
-        ),
       ),
+      padding: const EdgeInsets.all(10),
+      child: Image.asset('assets/images/logo_2.png', fit: BoxFit.contain),
     );
   }
 }
